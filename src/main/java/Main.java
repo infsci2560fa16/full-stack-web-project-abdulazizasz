2822d26a2426ca5d84ec091e9e339e911c25003b
Abdulaziz <aaa169@pitt.edu>
2016-11-06 17:31:03 -0500

    home

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,20 +22,19 @@ public class Main {
     externalStaticFileLocation("/public/index.html");
 
 
-    get("/", (request, response) -> new ModelAndView(new HashMap(), "/public/index.html"),
-            new FreeMarkerEngine());
+
 
     get("/hello", (req, res) -> "Hello World");
 
 
 
 
-//    get("/", (request, response) -> {
-//            Map<String, Object> attributes = new HashMap<>();
-//            attributes.put("message", "Hello World!");
-//
-//            return new ModelAndView(attributes, "index.ftl");
-//        }, new FreeMarkerEngine());
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
 
     get("/db", (req, res) -> {
       Connection connection = null;
